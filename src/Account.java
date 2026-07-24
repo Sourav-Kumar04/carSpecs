2e5b5121a18eb42128e9a1b9c3e2a92606821a2d
Sourav-Kumar04 <souravkumarverma2004@gmail.com>
2026-07-24 05:50:18 +0000

    created TestAcc

--- src/Account.java
+++ src/Account.java
@@ -1,3 +1,4 @@
+
 public class Account {
 
     int accountNumber;
@@ -56,6 +57,7 @@ public class Account {
         return withdraw(20);
     }
 
+
     @Override
     public String toString() {
         return "Account{" +
@@ -72,50 +74,5 @@ public class Account {
     @Override
     public boolean equals(Object obj) {
         return super.equals(obj);
-    }
-
-    public static void main(String[] args) {
-
-//        Account account = new Account();
-//        account.setAccountNumber(123456);
-//        account.setAccountBalance(1000);
-//
-//        System.out.println("Account Number: " + account.getAccountNumber());
-//        System.out.println("Account Balance: " + account.getAccountBalance());
-//
-//        account.withdraw(21);
-//        account.withdraw();
-
-        Account[] accounts = new Account[5];
-        accounts[0] = new Account(1000.0, 1);
-        accounts[1] = new Account(2000.0, 2);
-        accounts[2] = new Account(1500.0, 2);
-        accounts[3] = new Account(1500.0, 4);
-        accounts[4] = new Account(2500.0, 5);
-
-        Account.setInterestRate(0.07);
-
-        for (int i = 0; i < accounts.length; i++) {
-            System.out.println("Account: " + accounts[i]);
-            accounts[i].addInterest();
-            System.out.println("Balance after interest: " + accounts[i].getAccountBalance());
-            System.out.println();
-        }
-
-        System.out.println(accounts[2].equals(accounts[3]) ? "account 2 and 3 are equal" : "account 2 and 3 are not equal");
-        System.out.println(accounts[2].equals(accounts[4]) ? "account 2 and 4 are equal" : "account 2 and 4 are not equal");
-
-        accounts[0].withdraw(100);
-        accounts[1].withdraw();
-        accounts[2].withdraw(2500);
-
-
-        CarParts part = CarParts.ENGINE;
-
-        if(part == CarParts.ENGINE){
-            System.out.println("The part is an engine");
-        } else {
-            System.out.println("The part is not an engine");
-        }
     }
 }
--- src/TestAccount.java
+++ src/TestAccount.java
@@ -1,21 +1,44 @@
-// public class TestAccount {
-//    public static void main(String[] args) {
-//        Account[] accounts = new Account[5];
-//        accounts[0] = new Account(1000.0, "Alice");
-//        accounts[1] = new Account(2000.0, "Bob");
-//        accounts[2] = new Account(1500.0, "Charlie");
-//        accounts[3] = new Account(3000.0, "Diana");
-//        accounts[4] = new Account(2500.0, "Eve");
-//
-//        Account.setInterestRate(0.07);
-//
-//        for (int i = 0; i < accounts.length; i++) {
-//            System.out.println("Account Name: " + accounts[i].getName());
-//            System.out.println("Initial Balance: " + accounts[i].getBalance());
-//            accounts[i].addInterest();
-//            System.out.println("Balance after interest: " + accounts[i].getBalance());
-//            System.out.println();
-//        }
-//    }
-//}{
-//}
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestAccount {
+    public static void main(String[] args) {
+
+        Account[] accounts = new Account[5];
+        accounts[0] = new Account(1000.0, 1);
+        accounts[1] = new Account(2000.0, 2);
+        accounts[2] = new Account(1500.0, 3);
+        accounts[3] = new Account(1500.0, 4);
+        accounts[4] = new Account(2500.0, 5);
+
+        Account.setInterestRate(0.07);
+
+        for (int i = 0; i < accounts.length; i++) {
+            System.out.println("Account: " + accounts[i]);
+            accounts[i].addInterest();
+            System.out.println("Balance after interest: " + accounts[i].getAccountBalance());
+            System.out.println();
+        }
+
+        System.out.println(accounts[2].equals(accounts[3]) ? "account 2 and 3 are equal" : "account 2 and 3 are not equal");
+        System.out.println(accounts[2].equals(accounts[4]) ? "account 2 and 4 are equal" : "account 2 and 4 are not equal");
+
+        accounts[0].withdraw(100);
+        accounts[1].withdraw();
+        accounts[2].withdraw(2500);
+
+        // CarParts enum test
+        CarParts part = CarParts.WHEELS;
+        if (part == CarParts.ENGINE) {
+            System.out.println("The part is an engine");
+        } else {
+            System.out.println("The part is not an engine");
+        }
+
+        // List usage
+        List<Account> acc = new ArrayList<>();
+        acc.add(accounts[0]);
+        acc.add(accounts[1]);
+        System.out.println("Accounts in list: " + acc);
+    }
+}
